55c1909f149e62c26b512fac882e6063a93c4c05
SetyoRian <setyo.wahyu.trianto@gmail.com>
2023-04-11 13:41:52 +0700

    new

--- simulation.py
+++ simulation.py
@@ -4,6 +4,8 @@ from cv2 import dnn_superres
 import torch
 import os
 
+indexing = '5'
+
 
 def deleteAll():
     firstIndex = 5
@@ -11,7 +13,8 @@ def deleteAll():
     for first in range(firstIndex):
         for second in range(secondIndex):
             # Read image
-            image_name = 'benur_0' + str(first + 1) + '_0' + str(second + 1) + '.png'
+            image_name = 'DataSelect/PL-3-260/benur' + indexing + '_0' + str(first + 1) + '_0' + str(
+                second + 1) + '.png'
             os.remove(image_name)
 
 
@@ -31,11 +34,18 @@ def upscale():
     for first in range(firstIndex):
         for second in range(secondIndex):
             # Read image
-            image_name = 'benur_0' + str(first + 1) + '_0' + str(second + 1) + '.png'
+            image_name = 'DataSelect/PL-3-260/benur' + indexing + '_0' + str(first + 1) + '_0' + str(
+                second + 1) + '.png'
             image = cv2.imread(image_name)
+
+            scale_percent = 400  # percent of original size
+            width = int(image.shape[1] * scale_percent / 100)
+            height = int(image.shape[0] * scale_percent / 100)
+            dim = (width, height)
 
             # Upscale the image
             result = sr.upsample(image)
+            # result = cv2.resize(image, dim, interpolation=cv2.INTER_LINEAR)
             cv2.imwrite(image_name, result)
             print(image_name)
 
@@ -43,32 +53,32 @@ def upscale():
 cap = cv2.VideoCapture(r"E:\PENS\PA\Dataset-31-Juli-2022\PL-3-80 (2).mp4")
 # cap = cv2.VideoCapture(r"E:\PENS\PA\Dataset-2022-Alfany\PL-3-200-1.mp4")
 
-if not cap.isOpened():
-    print("Error opening the video file. Please double check your file path for typos. Or move the movie file to the "
-          "same location as this script/notebook")
+# if not cap.isOpened():
+#     print("Error opening the video file. Please double check your file path for typos. Or move the movie file to the "
+#           "same location as this script/notebook")
+#
+# # While the video is opened
+# while cap.isOpened():
+#     # Read the video file.
+#     ret, frame = cap.read()
+#     # If we got frames show them.
+#     if ret:
+#         # time.sleep(1/fps)
+#         cv2.imshow('frame', frame)
+#         if cv2.waitKey(1) & 0xFF == ord('c'):
+#             frame = frame[64:964, 507:1407, :]
+#             cv2.imwrite("benur.jpg", frame)
+#             break
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     # Or automatically break this whole loop if the video is over.
+#     else:
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
 
-# While the video is opened
-while cap.isOpened():
-    # Read the video file.
-    ret, frame = cap.read()
-    # If we got frames show them.
-    if ret:
-        # time.sleep(1/fps)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('c'):
-            frame = frame[64:964, 507:1407, :]
-            cv2.imwrite("benur.jpg", frame)
-            break
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    # Or automatically break this whole loop if the video is over.
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-tiles = image_slicer.slice('benur.jpg', 25)
+tiles = image_slicer.slice('DataSelect/PL-3-260/benur' + indexing + '.jpg', 25)
 
 print("Upscaling...")
 upscale()
@@ -79,14 +89,16 @@ model = torch.hub.load('.', 'custom',
                        path='/Users/asus/PycharmProjects/finalProject-training/yolov5-master/runs/train/YOLOv5s/weights/best.pt',
                        source='local')
 
-batchImg = ('benur_01_01.png', 'benur_01_02.png', 'benur_01_03.png', 'benur_01_04.png', 'benur_01_05.png',
-            'benur_02_01.png', 'benur_02_02.png', 'benur_02_03.png', 'benur_02_04.png', 'benur_02_05.png',
-            'benur_03_01.png', 'benur_03_02.png', 'benur_03_03.png', 'benur_03_04.png', 'benur_03_05.png',
-            'benur_04_01.png', 'benur_04_02.png', 'benur_04_03.png', 'benur_04_04.png', 'benur_04_05.png',
-            'benur_05_01.png', 'benur_05_02.png', 'benur_05_03.png', 'benur_05_04.png', 'benur_05_05.png',
-            )
+batchImg = (
+    'benur' + indexing + '_01_01.png', 'benur' + indexing + '_01_02.png', 'benur' + indexing + '_01_03.png', 'benur' + indexing + '_01_04.png', 'benur' + indexing + '_01_05.png',
+    'benur' + indexing + '_02_01.png', 'benur' + indexing + '_02_02.png', 'benur' + indexing + '_02_03.png', 'benur' + indexing + '_02_04.png', 'benur' + indexing + '_02_05.png',
+    'benur' + indexing + '_03_01.png', 'benur' + indexing + '_03_02.png', 'benur' + indexing + '_03_03.png', 'benur' + indexing + '_03_04.png', 'benur' + indexing + '_03_05.png',
+    'benur' + indexing + '_04_01.png', 'benur' + indexing + '_04_02.png', 'benur' + indexing + '_04_03.png', 'benur' + indexing + '_04_04.png', 'benur' + indexing + '_04_05.png',
+    'benur' + indexing + '_05_01.png', 'benur' + indexing + '_05_02.png', 'benur' + indexing + '_05_03.png', 'benur' + indexing + '_05_04.png', 'benur' + indexing + '_05_05.png',
+)
 
-path = '/Users/asus/PycharmProjects/finalProject-training/yolov5-master/'
+path = '/Users/asus/PycharmProjects/finalProject-training/yolov5-master/DataSelect/PL-3-260/'
+path2 = 'E:\\PENS\\PA\\Dataset-31-Juli-2022\\DataSelect\\PL-3-80\\benur'
 imgs = [path + f for f in batchImg]  # batch of images
 # Inference
 results = model(imgs, size=720)
